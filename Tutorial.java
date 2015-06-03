3a94923402d444fc76b989a77c0d34b22b75f741
dgirerd <danielgirerd@gmail.com>
2015-06-02 17:01:41 -0700

    Comments cleaning

--- Tutorial.java
+++ Tutorial.java
@@ -15,14 +15,5 @@ public class Tutorial extends Screen
         if(mouse.getX() >= 207 && mouse.getX() <= 392 && mouse.getY() >= 513 && mouse.getY() <= 572) {
                 Greenfoot.setWorld(new Menu());
         }
-        /*if (screen < NUM_SCREENS && mouse.getY() >= 520 && mouse.getY() <= 555) {
-            if (mouse.getX() >= 70 && mouse.getX() <= 265) {
-                Greenfoot.setWorld(new Menu());
-            } else if (mouse.getX() >= 410 && mouse.getX() <= 540) {
-                setBackground("images/Tutorial" + (++screen) + ".png");
-            }
-        } else if (screen == NUM_SCREENS && mouse.getX() >= 360 && mouse.getX() <= 555 && mouse.getY() >= 530 && mouse.getY() <= 555) {
-            Greenfoot.setWorld(new Menu());
-        }*/
     }
 }
